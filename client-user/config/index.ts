9d2613d9fb812642c7532cbff1e4995283e42b78
Kevin0412 <kevinzhu633@gmail.com>
2026-02-15 14:01:25 +0800

    docs: taro编译及预览流程

---
+++ client-user/config/index.ts
@@ -0,0 +1,78 @@
+import path from 'path'
+import type { UserConfigExport } from '@tarojs/cli'
+
+export default {
+  projectName: 'easy-stay-user',
+  date: '2024-2-14',
+  designWidth: 750,
+  deviceRatio: {
+    640: 2.34 / 2,
+    750: 1,
+    375: 2,
+    828: 1.81 / 2
+  },
+  sourceRoot: 'src',
+  outputRoot: 'dist',
+  plugins: ['@tarojs/plugin-framework-react'],
+  defineConstants: {},
+  copy: {
+    patterns: [],
+    options: {}
+  },
+  framework: 'react',
+  compiler: 'webpack5',
+  cache: {
+    enable: false
+  },
+  mini: {
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {}
+      },
+      url: {
+        enable: true,
+        config: {
+          limit: 1024
+        }
+      },
+      cssModules: {
+        enable: false,
+        config: {
+          namingPattern: 'module',
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    }
+  },
+  h5: {
+    publicPath: '/',
+    staticDirectory: 'static',
+    output: {
+      filename: 'js/[name].[hash:8].js',
+      chunkFilename: 'js/[name].[chunkhash:8].js'
+    },
+    miniCssExtractPluginOption: {
+      ignoreOrder: true,
+      filename: 'css/[name].[hash:8].css',
+      chunkFilename: 'css/[name].[chunkhash:8].css'
+    },
+    postcss: {
+      autoprefixer: {
+        enable: true,
+        config: {}
+      },
+      cssModules: {
+        enable: false,
+        config: {
+          namingPattern: 'module',
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    },
+    devServer: {
+      port: 10086,
+      host: '0.0.0.0'  // 允许局域网访问
+    }
+  }
+} satisfies UserConfigExport
--- client-user/src/app.config.ts
+++ client-user/src/app.config.ts
@@ -10,30 +10,5 @@ export default defineAppConfig({
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: '易宿酒店',
     navigationBarTextStyle: 'black'
-  },
-  tabBar: {
-    color: '#666',
-    selectedColor: '#1890ff',
-    backgroundColor: '#fff',
-    list: [
-      {
-        pagePath: 'pages/home/index',
-        text: '首页',
-        iconPath: 'assets/icons/home.png',
-        selectedIconPath: 'assets/icons/home-active.png'
-      },
-      {
-        pagePath: 'pages/list/index',
-        text: '酒店',
-        iconPath: 'assets/icons/hotel.png',
-        selectedIconPath: 'assets/icons/hotel-active.png'
-      },
-      {
-        pagePath: 'pages/profile/index',
-        text: '我的',
-        iconPath: 'assets/icons/profile.png',
-        selectedIconPath: 'assets/icons/profile-active.png'
-      }
-    ]
   }
 })
