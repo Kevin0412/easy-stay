7040b065d1e3c31d5d9e92bcefb19c1240b13b56
Kevin0412 <kevinzhu633@gmail.com>
2026-02-26 19:47:11 +0800

    feat: 添加测试组件用于演示Git提交流程

---
+++ client-admin/src/components/test-component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestComponent: React.FC = () => {
+  return (
+    <div>
+      <h1>Test Component</h1>
+      <p>这是一个用于演示Git提交的测试组件。</p>
+    </div>
+  );
+};
+
+export default TestComponent;
